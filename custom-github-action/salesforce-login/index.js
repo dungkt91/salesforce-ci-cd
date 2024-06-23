d25e4437cf920713a6270092dd93cd31ba307b63
Vu Van Dung <vandungktvn@gmail.com>
2024-06-23 11:56:54 +0700

    Update

--- custom-github-action/salesforce-login/index.js
+++ custom-github-action/salesforce-login/index.js
@@ -7,7 +7,7 @@ const password = core.getInput('password');
 
 async function run(){
     let response = await fetch("http://www.google.com");
-    console.log("body: ", response.body());
+    console.log("body: ", await response.text());
 }
 
 run();
